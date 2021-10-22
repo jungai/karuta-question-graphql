cd6b1cf0e168dd72116eb66c61faa9fb50f57549
jungai <juworaphol125@gmail.com>
2021-10-22 15:15:14 +0700

    update mock database

--- database/1_table_structure_monton_jean.sql
+++ database/1_table_structure_monton_jean.sql
@@ -1,40 +1,61 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `jungai_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+-- -------------------------------------------------------------
+-- TablePlus 4.2.0(388)
+--
+-- https://tableplus.com/
+--
+-- Database: jungai_test
+-- Generation Time: 2564-10-22 15:08:53.3970
+-- -------------------------------------------------------------
 
 USE `jungai_test`;
 
-DROP TABLE IF EXISTS `monton_jean`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `monton_jean` (
-  `monton_id` int(10) NOT NULL AUTO_INCREMENT,
-  `monton_code` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `monton_name` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`monton_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP TABLE IF EXISTS `monton_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `monton_user` (
-  `user_id` int(10) NOT NULL AUTO_INCREMENT,
-  `monton_id` int(11) NOT NULL COMMENT 'Monton Jean Id',
-  `name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`user_id`) USING BTREE,
-  KEY `MontonJean` (`monton_id`) USING BTREE,
-  CONSTRAINT `UserNeedInMontonJean` FOREIGN KEY (`monton_id`) REFERENCES `jungai_test`.`monton_jean` (`monton_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `monton_idol`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `monton_idol` (
-  `idol_id` int(10) NOT NULL AUTO_INCREMENT,
-  `idol_name` varchar(50) NOT NULL DEFAULT '',
-  `user_id` int(11) NOT NULL COMMENT 'User Id',
-  PRIMARY KEY (`idol_id`) USING BTREE,
-  KEY `UserId` (`user_id`) USING BTREE,
-  CONSTRAINT `UserNeedFollowIdol` FOREIGN KEY (`user_id`) REFERENCES `jungai_test`.`monton_user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `Answer`;
+CREATE TABLE `Answer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qId` int(11) NOT NULL,
+  `cId` int(11) NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  KEY `Answer_qId_fkey` (`qId`),
+  KEY `Answer_cId_fkey` (`cId`),
+  CONSTRAINT `Answer_cId_fkey` FOREIGN KEY (`cId`) REFERENCES `Character` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `Answer_qId_fkey` FOREIGN KEY (`qId`) REFERENCES `Question` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `Character`;
+CREATE TABLE `Character` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Character_name_key` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `Question`;
+CREATE TABLE `Question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- database/2_static_data_monton_jean.sql
+++ database/2_static_data_monton_jean.sql
@@ -1,31 +1,33 @@
-SET NAMES utf8;
-
-INSERT INTO `jungai_test`.`monton_jean`(`monton_id`, `monton_code`, `monton_name`)
-VALUES
-(1, 'monton_01', 'ไทยกัว1'),
-(2, 'monton_02', 'ไทยกัว2'),
-(3, 'monton_03', 'ไทยกัว3'),
-(4, 'monton_04', 'ไทยกัว4'),
-(5, 'monton_05', 'ไทยกัว5'),
-(6, 'monton_06', 'ไทยกัว6'),
-(7, 'monton_07', 'ไทยกัว7'),
-(8, 'monton_08', 'ไทยกัว8'),
-(9, 'monton_09', 'ไทยกัว9'),
-(10, 'monton_10', 'ไทยกัว10');
-
-INSERT INTO `jungai_test`.`monton_user`(`user_id`, `monton_id`, `name`)
-VALUES
-(1, 1, 'park jae ju'),
-(2, 1, 'jungai'),
-(3, 2, 'kim long teeth'),
-(4, 3, 'kim bok juu');
+-- -------------------------------------------------------------
+-- TablePlus 4.2.0(388)
+--
+-- https://tableplus.com/
+--
+-- Database: jungai_test
+-- Generation Time: 2564-10-22 15:10:07.8200
+-- -------------------------------------------------------------
 
 
-INSERT INTO `jungai_test`.`monton_idol`(`idol_id`, `idol_name`, `user_id`)
-VALUES
-(1, 'iu', 1),
-(2, 'iu', 2),
-(3, 'iu', 3),
-(4, 'rose', 1),
-(5, 'rose', 2);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+INSERT INTO `jungai_test`.`Question` (`id`, `question`, `createdAt`) VALUES
+(1, 'What inconvenience would you rather deal with at home?', '2021-10-22 15:04:58.693'),
+(2, 'Compared to other characters from the series you know me from, how annoying am I?', '2021-10-22 15:06:22.220');
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
